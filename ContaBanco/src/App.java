cdf37b417811c28ec5751c354075fdcd194d8478
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2024-03-06 20:24:43 -0300

    initial version

---
+++ ContaBanco/src/App.java
@@ -0,0 +1,11 @@
+/** 
+ * @author Thiago S Gomes
+ * @version 1.0.0
+ * @since 06/03/2024
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
